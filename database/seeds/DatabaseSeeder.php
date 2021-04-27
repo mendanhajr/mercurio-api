cf513df45f8876e2eb70afff84e4bfcd3619634d
Mendanha <luizmendanhajr@gmail.com>
2021-04-27 15:47:25 -0300

    adicionado CatalogoDespesasTableSeeder em DatabaseSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,5 +13,6 @@ class DatabaseSeeder extends Seeder
     {
          $this->call(UsersTableSeeder::class);
          $this->call(TipoDespesasTableSeeder::class);
+         $this->call(CatalogoDespesasTableSeeder::class);
     }
 }
