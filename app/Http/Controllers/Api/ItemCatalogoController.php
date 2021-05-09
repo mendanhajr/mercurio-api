8b4f7add84f79c978d72a17f45de41219853b019
Mendanha <luizmendanhajr@gmail.com>
2021-05-09 14:11:03 -0300

    adicionado parametro na consulta de item e catalogo

--- app/Http/Controllers/Api/ItemCatalogoController.php
+++ app/Http/Controllers/Api/ItemCatalogoController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 use App\Models\ItemCatalogo;
 use App\Http\Controllers\MasterController;
+use Illuminate\Http\Request;
 
 class ItemCatalogoController extends MasterController
 {
@@ -12,6 +13,15 @@ class ItemCatalogoController extends MasterController
     public function __construct(ItemCatalogo $itemCatalogo)
     {
         $this->model = $itemCatalogo;
+    }
+
+    public function index(Request $request)
+    {
+        $data = $this->model::select('item_catalogo.*')
+            ->where('id_catalogo', $request->id_catalogo)
+            ->get();
+
+        return response()->json($data);
     }
 
 }
