7847898397e13b3af9e8ef6df8864ad847c922ec
Mendanha <luizmendanhajr@gmail.com>
2021-05-30 12:03:59 -0300

    inserido login para lower case

--- app/Http/Controllers/Auth/AuthenticateController.php
+++ app/Http/Controllers/Auth/AuthenticateController.php
@@ -20,6 +20,7 @@ class AuthenticateController extends Controller
     {
         // grab credentials from the request
         $credentials = $request->only('email', 'password');
+        $credentials['email'] = strtolower($credentials['email']);
 
         try {
             // attempt to verify the credentials and create a token for the user
